c264effc0133046215891f796d07f3fd553fc22a
bhawana@habrie.com-at-671004753547 <bhawna@habrie.com>
2024-09-11 18:50:00 +0530

    switch user

--- backend_admin/admin.py
+++ backend_admin/admin.py
@@ -1,22 +1,28 @@
-from django.http import HttpResponseRedirect
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
-from timed_auth_token.models import TimedAuthToken
+from django.http import HttpResponse
 from django.contrib.sessions.backends.db import SessionStore
+from django.contrib.auth import get_user_model
+from timed_auth_token.models import TimedAuthToken
+import json
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib import admin
+
 
 User = get_user_model()
 
+class HttpResponseRedirectWithData(HttpResponse):
+    def __init__(self, redirect_to, data, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self['Location'] = redirect_to
+        self['X-Custom-Data'] = json.dumps(data)
+        self.status_code = 302
+
 def switch_user(modeladmin, request, queryset):
     if queryset.count() != 1:
-        messages.error(request, "Please select exactly one user to switch.")
-        return JsonResponse({"error": "Please select exactly one user to switch."}, status=400)
+        return HttpResponse("Please select exactly one user to switch.", status=400)
     
     user = queryset.first()
-
     if not user.is_active:
-        messages.error(request, "Cannot switch to an inactive user.")
-        return JsonResponse({"error": "Cannot switch to an inactive user."}, status=400)
+        return HttpResponse("Cannot switch to an inactive user.", status=400)
 
     # Generate or retrieve the token
     token, _ = TimedAuthToken.objects.get_or_create(user=user)
@@ -36,10 +42,20 @@ def switch_user(modeladmin, request, queryset):
     # Construct the redirect URL with the session key and token
     redirect_url = f"https://staging.alfcoretrainingflorida.com/#/signin?session_key={session.session_key}&auth_token={token.key}"
 
-    # messages.success(request, f"Switched to user: {user.username}")
+    # Prepare additional data
+    additional_data = {
+        "user_data": {
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+        },
+        "session_key": session.session_key,
+        "auth_token": token.key
+    }
 
-    # Redirect to the URL with the session key and token
-    return HttpResponseRedirect(redirect_url)
+    # Return custom response
+    return HttpResponseRedirectWithData(redirect_url, additional_data)
 
 switch_user.short_description = "Switch to selected user"
 
